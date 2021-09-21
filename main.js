a7e1d4734951f53a63a42df2bf6a8e8dbb5b51ff
NhaVoVan <38065969+vvnha@users.noreply.github.com>
2021-09-21 16:20:08 +0700

    update

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ const glob = require('glob');
 const folder = 'D:/doc/*';
 const getFile = require('./woker');
 
+const file = glob.sync(folder);
+const temp = [];
+while (file.length) {
+    temp.push(file.splice(0, 10));
+}
+
 (async () => {
-    await Promise.all(glob.sync(folder)
-        .map(getFile));
+    let i = 0;
+    while (i < temp.length) {
+        await Promise.all(temp[i]
+            .map(getFile));
+        i++;
+    }
+
 })();
--- woker.js
+++ woker.js
@@ -1,4 +1,25 @@
 const { Worker, isMainThread, threadId, workerData, parentPort } = require('worker_threads');
+const getContent = require('./getContent');
+var difflib = require('difflib');
+const path1 = 'C:/Users/Admin/Downloads/BaoCaoThucTap.docx';
+
+const getContentFromPath = async (path) => {
+    var dot = path.lastIndexOf("/");
+    var name = path.substr(dot + 1, path.length);
+    let data = await getContent.getContent(path, name);
+    if (data === null || data.trim() === null) {
+        throw new Error('file empty!');
+        return;
+    }
+    return data.toLowerCase();
+}
+
+const getSimilitary = async (path2, path3) => {
+    let data1 = await getContentFromPath(path2);
+    let data2 = await getContentFromPath(path3);
+    var similarity = new difflib.SequenceMatcher(null, data1, data2).ratio();
+    return similarity;
+}
 
 if (isMainThread) {
     module.exports = (file) => new Promise((resolve, reject) => {
@@ -14,7 +35,9 @@ if (isMainThread) {
         })
     })
 } else {
-    console.log(`${threadId} starting ${workerData}`);
-    //console.log(workerData);
-    parentPort.postMessage(workerData);
+    (async () => {
+        const simi = await getSimilitary(path1, workerData);
+        console.log(simi + "~" + workerData)
+        parentPort.postMessage(workerData);
+    })();
 }
