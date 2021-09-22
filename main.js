b7557a716ec5c01d2d20cd5089f9a99964e81616
NhaVoVan <38065969+vvnha@users.noreply.github.com>
2021-09-22 08:44:36 +0700

    update code

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
 const glob = require('glob');
-const folder = 'D:/doc/*';
+const folder = 'C:/Users/Admin/OneDrive/Documents/thuctap/*';
 const getFile = require('./woker');
+const keyword_extractor = require("keyword-extractor");
 
-const file = glob.sync(folder);
-const temp = [];
-while (file.length) {
-    temp.push(file.splice(0, 10));
-}
 
-(async () => {
-    let i = 0;
-    while (i < temp.length) {
-        await Promise.all(temp[i]
-            .map(getFile));
-        i++;
-    }
+// folder = 'D:/doc/*;
 
-})();
+// const file = glob.sync(folder);
+// const temp = [];
+// while (file.length) {
+//     temp.push(file.splice(0, 10));
+// }
+
+// (async () => {
+//     let i = 0;
+//     while (i < temp.length) {
+//         await Promise.all(temp[i]
+//             .map(getFile));
+//         i++;
+//     }
+
+// })();
+
+const sentence =
+    "Chào Mừng thế giới, nè hẽ"
+
+//  Extract the keywords
+const extraction_result =
+    keyword_extractor.extract(sentence, {
+        language: "english",
+        remove_digits: true,
+        return_changed_case: true,
+        remove_duplicates: false
+    });
+
+const sortPriority = (arr) => arr.sort(function (a, b) {
+    return b.length - a.length;
+});
+
+console.log(sortPriority(extraction_result));
